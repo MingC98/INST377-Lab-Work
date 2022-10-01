4e0d6b026a9e1fca871995e15671eb6edc033676
MingC98 <102772564+MingC98@users.noreply.github.com>
2022-09-30 22:57:32 -0400

    5

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid dic'))
+    const width = 10
+
 });
